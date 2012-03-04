ad5f9bbdea13aff4ce0a26d50e9a41d7f8bf5909
makestuff <github.com@m3.ath.cx>
2012-03-04 16:18:01 +0000

    Switched to LGPL

--- release.sh
+++ release.sh
@@ -1,5 +1,21 @@
-# Helper script for building the binary distribution. It's unlikely you'll need this unless you're
-# forking the project.
+#
+# Copyright (C) 2009-2012 Chris McClelland
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Helper script for building the binary distribution. It's unlikely you'll need
+# this unless you're forking the project.
 #
 #!/bin/bash
 export LIB=libfx2loader
@@ -7,17 +23,29 @@ export DATE=$(date +%Y%m%d)
 rm -rf ${LIB}-${DATE}
 mkdir ${LIB}-${DATE}
 
+# Linux x86_64 binaries
+mkdir -p ${LIB}-${DATE}/linux.x86_64/rel
+cp -rp linux.x86_64/rel/*.so ${LIB}-${DATE}/linux.x86_64/rel/
+mkdir -p ${LIB}-${DATE}/linux.x86_64/dbg
+cp -rp linux.x86_64/dbg/*.so ${LIB}-${DATE}/linux.x86_64/dbg/
+
 # Linux i686 binaries
 mkdir -p ${LIB}-${DATE}/linux.i686/rel
 cp -rp linux.i686/rel/*.so ${LIB}-${DATE}/linux.i686/rel/
 mkdir -p ${LIB}-${DATE}/linux.i686/dbg
 cp -rp linux.i686/dbg/*.so ${LIB}-${DATE}/linux.i686/dbg/
 
-# Linux x86_64 binaries
-mkdir -p ${LIB}-${DATE}/linux.x86_64/rel
-cp -rp linux.x86_64/rel/*.so ${LIB}-${DATE}/linux.x86_64/rel/
-mkdir -p ${LIB}-${DATE}/linux.x86_64/dbg
-cp -rp linux.x86_64/dbg/*.so ${LIB}-${DATE}/linux.x86_64/dbg/
+# Linux armel binaries
+mkdir -p ${LIB}-${DATE}/linux.armel/rel
+cp -rp linux.armel/rel/*.so ${LIB}-${DATE}/linux.armel/rel/
+mkdir -p ${LIB}-${DATE}/linux.armel/dbg
+cp -rp linux.armel/dbg/*.so ${LIB}-${DATE}/linux.armel/dbg/
+
+# Linux ppc binaries
+mkdir -p ${LIB}-${DATE}/linux.ppc/rel
+cp -rp linux.ppc/rel/*.so ${LIB}-${DATE}/linux.ppc/rel/
+mkdir -p ${LIB}-${DATE}/linux.ppc/dbg
+cp -rp linux.ppc/dbg/*.so ${LIB}-${DATE}/linux.ppc/dbg/
 
 # MacOS binaries
 mkdir -p ${LIB}-${DATE}/darwin/rel
@@ -41,6 +69,10 @@ cp -rp ${FX2CLI}/linux.x86_64/rel/libargtable2.so ${FX2CLI}/linux.x86_64/rel/fx2
 cp -rp ${FX2CLI}/linux.x86_64/dbg/libargtable2.so ${FX2CLI}/linux.x86_64/dbg/fx2loader ${LIB}-${DATE}/linux.x86_64/dbg/
 cp -rp ${FX2CLI}/linux.i686/rel/libargtable2.so ${FX2CLI}/linux.i686/rel/fx2loader ${LIB}-${DATE}/linux.i686/rel/
 cp -rp ${FX2CLI}/linux.i686/dbg/libargtable2.so ${FX2CLI}/linux.i686/dbg/fx2loader ${LIB}-${DATE}/linux.i686/dbg/
+cp -rp ${FX2CLI}/linux.armel/rel/libargtable2.so ${FX2CLI}/linux.armel/rel/fx2loader ${LIB}-${DATE}/linux.armel/rel/
+cp -rp ${FX2CLI}/linux.armel/dbg/libargtable2.so ${FX2CLI}/linux.armel/dbg/fx2loader ${LIB}-${DATE}/linux.armel/dbg/
+cp -rp ${FX2CLI}/linux.ppc/rel/libargtable2.so ${FX2CLI}/linux.ppc/rel/fx2loader ${LIB}-${DATE}/linux.ppc/rel/
+cp -rp ${FX2CLI}/linux.ppc/dbg/libargtable2.so ${FX2CLI}/linux.ppc/dbg/fx2loader ${LIB}-${DATE}/linux.ppc/dbg/
 cp -rp ${FX2CLI}/darwin/rel/libargtable2.dylib ${FX2CLI}/darwin/rel/fx2loader ${LIB}-${DATE}/darwin/rel/
 cp -rp ${FX2CLI}/darwin/dbg/libargtable2.dylib ${FX2CLI}/darwin/dbg/fx2loader ${LIB}-${DATE}/darwin/dbg/
 cp -rp ${FX2CLI}/win32/rel/argtable2.dll ${FX2CLI}/win32/rel/fx2loader.exe ${LIB}-${DATE}/win32/rel/
@@ -55,7 +87,7 @@ cp -rp ../../common/makestuff.h ${LIB}-${DATE}/
 cp -rp ../libbuffer/libbuffer.h ${LIB}-${DATE}/
 cp -rp ${LIB}.h ${LIB}-${DATE}/
 
-cp -p LICENSE.txt ${LIB}-${DATE}/
+cp -p COPYING COPYING.LESSER ${LIB}-${DATE}/
 cat > ${LIB}-${DATE}/README <<EOF
 FX2Loader Binary Distribution
 
